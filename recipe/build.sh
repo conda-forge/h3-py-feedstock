43254b6407689f59daaef0ff423500c5e9e54095
Lindsey Gray <lindsey.gray@gmail.com>
2025-06-30 17:22:29 -0500

    use h3 from conda

--- recipe/build.sh
+++
@@ -1,9 +0,0 @@
-# override scikit-build handling for macOS arm platforn on conda-forge cross-compiling
-if [ "$target_platform" = "osx-arm64" ]; then
-  export CMAKE_OSX_ARCHITECTURES="arm64"
-  # workaround for https://github.com/scikit-build/scikit-build/issues/589
-  rm -rf $PREFIX/lib/libpython$PY_VER.dylib
-  ln -sf $PREFIX/lib/libc++.dylib $PREFIX/lib/libpython$PY_VER.dylib
-fi
-
-$PYTHON -m pip install . -vv
